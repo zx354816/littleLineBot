6b0731f012abe5965a02877bdd8e657622a88d69
Ying-Chieh Chao <family.chao@gmail.com>
2017-12-04 02:00:44 +0800

    make it better

--- index.js
+++ index.js
@@ -59,12 +59,102 @@ app.post('/linewebhook', parser, function (req, res) {
 	return res.json({});
 });
 
+// 簡單回應訊息
+// bot.on('message', function (event) {
+// 	event.reply(event.message.text).then(function (data) {
+// 		console.log('Success', data);
+// 	}).catch(function (error) {
+// 		console.log('Error', error);
+// 	});
+// });
+
 bot.on('message', function (event) {
-	event.reply(event.message.text).then(function (data) {
-		console.log('Success', data);
-	}).catch(function (error) {
-		console.log('Error', error);
-	});
+	switch (event.message.type) {
+		case 'text':
+			switch (event.message.text) {
+				case 'Me':
+					event.source.profile().then(function (profile) {
+						return event.reply('Hello ' + profile.displayName + ' ' + profile.userId);
+					});
+					break;
+				case 'Picture':
+					event.reply({
+						type: 'image',
+						originalContentUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png',
+						previewImageUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png'
+					});
+					break;
+				case 'Location':
+					event.reply({
+						type: 'location',
+						title: 'LINE Plus Corporation',
+						address: '1 Empire tower, Sathorn, Bangkok 10120, Thailand',
+						latitude: 13.7202068,
+						longitude: 100.5298698
+					});
+					break;
+				case 'Confirm':
+					event.reply({
+						type: 'template',
+						altText: 'this is a confirm template',
+						template: {
+							type: 'confirm',
+							text: 'Are you sure?',
+							actions: [{
+								type: 'message',
+								label: 'Yes',
+								text: 'yes'
+							}, {
+								type: 'message',
+								label: 'No',
+								text: 'no'
+							}]
+						}
+					});
+					break;
+				case 'Multiple':
+					return event.reply(['Line 1', 'Line 2', 'Line 3', 'Line 4', 'Line 5']);
+					break;
+				case 'Version':
+					event.reply('linebot@' + require('../package.json').version);
+					break;
+				default:
+					event.reply(event.message.text).then(function (data) {
+						console.log('Success', data);
+					}).catch(function (error) {
+						console.log('Error', error);
+					});
+					break;
+			}
+			break;
+		case 'image':
+			event.message.content().then(function (data) {
+				const s = data.toString('base64').substring(0, 30);
+				return event.reply('Nice picture! ' + s);
+			}).catch(function (err) {
+				return event.reply(err.toString());
+			});
+			break;
+		case 'video':
+			event.reply('Nice movie!');
+			break;
+		case 'audio':
+			event.reply('Nice song!');
+			break;
+		case 'location':
+			event.reply(['That\'s a good location!', 'Lat:' + event.message.latitude, 'Long:' + event.message.longitude]);
+			break;
+		case 'sticker':
+			event.reply({
+				type: 'sticker',
+				packageId: 1,
+				stickerId: 1
+			});
+			break;
+		default:
+			event.reply('Unknow message: ' + JSON.stringify(event));
+			break;
+	}
 });
 
 app.listen(process.env.PORT || 80, function () {
