bf1d2b9067c9d69111cc4480c90efb897048e776
Ying-Chieh Chao <family.chao@gmail.com>
2017-12-04 02:38:02 +0800

    make it better

--- index.js
+++ index.js
@@ -77,6 +77,14 @@ bot.on('message', function (event) {
 						return event.reply('Hello ' + profile.displayName + ' ' + profile.userId);
 					});
 					break;
+				case '空氣':
+					getAQI();
+					emitter.on ('aqiEvent', function (data) {
+						event.reply('\uDBC0\uDC84 ' + data.County + data.SiteName +
+						'PM2.5指數：'+ data["PM2.5_AVG"] + 
+					    '狀態：' + data.Status);
+					});
+					break;
 				case 'Picture':
 					event.reply({
 						type: 'image',
