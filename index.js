8dddaf6f4e2a3d1f97fc10902b7cb3f3444d566b
Ying-Chieh Chao <family.chao@gmail.com>
2017-12-07 10:03:11 +0800

    make it better

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
 const linebot = require('linebot');
 const express = require('express');
-var rp = require('request-promise');
-const bodyParser = require('body-parser');
-
-const SITE_NAME = '西屯';
-var aqiOpt = {
-    uri: "http://opendata2.epa.gov.tw/AQI.json",
-    json: true // Automatically parses the JSON string in the response
-}; 
-
-function readAQI(repos){
-    let data;
-    
-    for (i in repos) {
-        if (repos[i].SiteName == SITE_NAME) {
-            data = repos[i];
-            break;
-        }
-    }
-
-    return data;
-}
 
 const bot = linebot({
 	channelId: process.env.CHANNEL_ID,
@@ -29,142 +8,21 @@ const bot = linebot({
 });
 
 const app = express();
-app.set('view engine', 'ejs');
 
-const parser = bodyParser.json({
-	verify: function (req, res, buf, encoding) {
-		req.rawBody = buf.toString(encoding);
-	}
-});
+const linebotParser = bot.parser();
 
 app.get('/',function(req,res){
-    rp(aqiOpt)
-    .then(function (repos) {
-        res.render('index', {AQI:readAQI(repos)});
-    })
-    .catch(function (err) {
-		// API call failed...
-		res.send("error...");
-    });
+    res.send('Hello World!');
 });
 
-app.post('/linewebhook', parser, function (req, res) {
-	if (!bot.verify(req.rawBody, req.get('X-Line-Signature'))) {
-		return res.sendStatus(400);
-	}
-	bot.parse(req.body);
-	return res.json({});
-});
-
-// 簡單回應訊息
-// bot.on('message', function (event) {
-// 	event.reply(event.message.text).then(function (data) {
-// 		console.log('Success', data);
-// 	}).catch(function (error) {
-// 		console.log('Error', error);
-// 	});
-// });
+app.post('/linewebhook', linebotParser);
 
 bot.on('message', function (event) {
-	switch (event.message.type) {
-		case 'text':
-			switch (event.message.text) {
-				case 'Me':
-					event.source.profile().then(function (profile) {
-						return event.reply('Hello ' + profile.displayName + ' ' + profile.userId);
-					});
-					break;
-				case '空氣':
-					let data;
-					rp(aqiOpt)
-					.then(function (repos) {
-						data = readAQI(repos);
-						event.reply('\uDBC0\uDC84 ' + data.County + data.SiteName +
-						'\n\nPM2.5指數：'+ data["PM2.5_AVG"] + 
-					    '\n狀態：' + data.Status);
-					})
-					.catch(function (err) {
-						event.reply('\uDBC0\uDC84 ' + "接收空氣品質資料時發生問題了～");
-					});
-					break;
-				case 'Picture':
-					event.reply({
-						type: 'image',
-						originalContentUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png',
-						previewImageUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png'
-					});
-					break;
-				case 'Location':
-					event.reply({
-						type: 'location',
-						title: 'LINE Plus Corporation',
-						address: '1 Empire tower, Sathorn, Bangkok 10120, Thailand',
-						latitude: 13.7202068,
-						longitude: 100.5298698
-					});
-					break;
-				case 'Confirm':
-					event.reply({
-						type: 'template',
-						altText: 'this is a confirm template',
-						template: {
-							type: 'confirm',
-							text: 'Are you sure?',
-							actions: [{
-								type: 'message',
-								label: 'Yes',
-								text: 'yes'
-							}, {
-								type: 'message',
-								label: 'No',
-								text: 'no'
-							}]
-						}
-					});
-					break;
-				case 'Multiple':
-					return event.reply(['Line 1', 'Line 2', 'Line 3', 'Line 4', 'Line 5']);
-					break;
-				case 'Version':
-					event.reply('linebot@' + require('../package.json').version);
-					break;
-				default:
-					event.reply(event.message.text).then(function (data) {
-						console.log('Success', data);
-					}).catch(function (error) {
-						console.log('Error', error);
-					});
-					break;
-			}
-			break;
-		case 'image':
-			event.message.content().then(function (data) {
-				const s = data.toString('base64').substring(0, 30);
-				return event.reply('Nice picture! ' + s);
-			}).catch(function (err) {
-				return event.reply(err.toString());
-			});
-			break;
-		case 'video':
-			event.reply('Nice movie!');
-			break;
-		case 'audio':
-			event.reply('Nice song!');
-			break;
-		case 'location':
-			event.reply(['That\'s a good location!', 'Lat:' + event.message.latitude, 'Long:' + event.message.longitude]);
-			break;
-		case 'sticker':
-			event.reply({
-				type: 'sticker',
-				packageId: 1,
-				stickerId: 1
-			});
-			break;
-		default:
-			event.reply('Unknow message: ' + JSON.stringify(event));
-			break;
-	}
+	event.reply(event.message.text).then(function (data) {
+		console.log('Success', data);
+	}).catch(function (error) {
+		console.log('Error', error);
+	});
 });
 
 app.listen(process.env.PORT || 80, function () {
