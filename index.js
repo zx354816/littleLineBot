4c74db78271a45a09436d783157bf2a452d81e89
zx354816 <qap66990070@gmail.com>
2019-07-20 16:26:34 +0800

    make it

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const linebot = require('linebot');
 const express = require('express');
+const fs = require("fs");
 
 const bot = linebot({
 	channelId: process.env.CHANNEL_ID,
@@ -17,12 +18,100 @@ app.get('/',function(req,res){
 
 app.post('/linewebhook', linebotParser);
 
+
+function limitRandomNumber(n, m) {
+	var c = m - n + 1;
+	return Math.floor(Math.random() * c + n);
+}
+
+
+var FoodList = ['711','弘爺'];
+
+
+
 bot.on('message', function (event) {
-	event.reply(event.message.text).then(function (data) {
-		console.log('Success', data);
-	}).catch(function (error) {
-		console.log('Error', error);
-	});
+	if (event.message.text.match('吃啥') || event.message.text.match('吃什麼') || event.message.text.match('吃甚麼') != null) {
+		fs.readFile('food.txt', function (err, data) {
+			if (err) throw err;
+		 
+			console.log(data.toString());
+			FoodList = data.toString();
+		});
+
+
+		
+		var ListLength = FoodList.length;
+		event.reply(FoodList[limitRandomNumber(0, ListLength - 1)]).then(function (data) {
+			// success 
+			console.log(msg);
+		}).catch(function (error) {
+			// error 
+			console.log('error');
+		});
+		
+	}
+	else if (event.message.text.match('新增餐廳:') != null || event.message.text.match('新增餐廳：') != null) {
+
+		var newString = event.message.text.substring(5);
+		if (FoodList.indexOf(newString) == -1) {
+
+			FoodList.push(newString);
+			event.reply('已新增' + newString + '。').then(function (data) {
+				// success 
+				console.log(msg);
+			}).catch(function (error) {
+				// error 
+				console.log('error');
+			});
+
+			
+		}
+		else {
+			event.reply('裡面已經有這個了啦').then(function (data) {
+				// success 
+				console.log(msg);
+			}).catch(function (error) {
+				// error 
+				console.log('error');
+			});
+		}
+	}
+	else if (event.message.text.match('移除餐廳:') != null || event.message.text.match('移除餐廳：') != null) {
+
+		var newString = event.message.text.substring(5);
+		if (FoodList.indexOf(newString) != -1) {
+
+			var newnewString = FoodList.splice(FoodList.indexOf(newString), 1);
+
+			event.reply('已移除' + newnewString + '。').then(function (data) {
+				// success 
+				console.log(msg);
+			}).catch(function (error) {
+				// error 
+				console.log('error');
+			});
+		} else {
+			event.reply('裡面沒有這間啦').then(function (data) {
+				// success 
+				console.log(msg);
+			}).catch(function (error) {
+				// error 
+				console.log('error');
+			});
+		}
+	}
+	else if (event.message.text == '全部的餐廳' || event.message.text == '所有餐廳' || event.message.text == '全部餐廳' || event.message.text == '所有的餐廳') {
+
+		var _all = FoodList.join('、').toString();
+
+		event.reply(_all).then(function (data) {
+			// success 
+			console.log(msg);
+		}).catch(function (error) {
+			// error 
+			console.log('error');
+		});
+	}
 });
 
 app.listen(process.env.PORT || 80, function () {
